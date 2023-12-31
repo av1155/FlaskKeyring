b6ecc332efc659fdbe9a2b4a87f19808f865d702
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2023-12-31 05:05:17 -0500

    (feat): Added edit, delete, search, and view

--- app.py
+++ app.py
@@ -63,11 +63,11 @@ def load_user(user_id):
 @app.route("/")
 def index():
     current_year = datetime.now().year
-
     if current_user.is_authenticated:
+        passwords = Password.query.filter_by(user_id=current_user.id).all()
         return render_template(
-            "dashboard.html", current_year=current_year
-        )  # Dashboard for logged-in users
+            "dashboard.html", current_year=current_year, passwords=passwords
+        )
     return render_template("index.html")  # Public landing page
 
 
@@ -116,7 +116,10 @@ def logout():
 @login_required
 def dashboard():
     current_year = datetime.now().year
-    return render_template("dashboard.html", current_year=current_year)
+    passwords = Password.query.filter_by(user_id=current_user.id).all()
+    return render_template(
+        "dashboard.html", current_year=current_year, passwords=passwords
+    )
 
 
 @app.route("/add_password", methods=["GET", "POST"])
@@ -145,11 +148,29 @@ def add_password():
     return render_template("dashboard.html")
 
 
-@app.route("/view_passwords")
+@app.route("/view_password/<int:password_id>")
 @login_required
-def view_passwords():
-    passwords = Password.query.filter_by(user_id=current_user.id).all()
-    return render_template("view_passwords.html", passwords=passwords)
+def view_password(password_id):
+    password = Password.query.get_or_404(password_id)
+    return render_template("view_password.html", password=password)
+
+
+@app.route("/search_password", methods=["POST"])
+@login_required
+def search_password():
+    search_website = request.form.get("search_website")
+    password = Password.query.filter_by(
+        user_id=current_user.id, website=search_website
+    ).first()
+
+    if not password:
+        return render_template(
+            "view_passwords.html",
+            password=None,
+            message="Password not found for the given website.",
+        )
+
+    return render_template("view_passwords.html", password=password)
 
 
 @app.route("/edit_password/<int:password_id>", methods=["GET", "POST"])
@@ -164,7 +185,7 @@ def edit_password(password_id):
         password.password = request.form.get("password")
         db.session.commit()
 
-        return redirect(url_for("view_passwords"))
+        return redirect(url_for("dashboard"))
 
     return render_template("edit_password.html", password=password)
 
@@ -175,7 +196,7 @@ def remove_password(password_id):
     password = Password.query.get_or_404(password_id)
     db.session.delete(password)
     db.session.commit()
-    return redirect(url_for("view_passwords"))
+    return redirect(url_for("dashboard"))
 
 
 if __name__ == "__main__":
