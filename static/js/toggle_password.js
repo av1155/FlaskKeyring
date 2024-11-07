8b8783ba1afdcac8f4fd381102e0adf0e966d4c4
av1155 <117413846+av1155@users.noreply.github.com>
2024-11-06 23:03:04 -0500

    Enhance UI, Security, and Add Legal Pages
    - Updated CSP policy and LICENSE contact email for better security and
    compliance.
    - Refined password toggle functionality and updated password input
    fields in multiple templates.
    - Enhanced styling for forms, hero, and footer sections in styles.css.
    - Added Terms of Service and Privacy Policy routes and templates.
    - Improved container styling and streamlined redundant HTML code across
    templates.

--- static/js/toggle_password.js
+++ static/js/toggle_password.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const passwordInput = document.getElementById("floatingPassword");
-    const togglePasswordButton = document.getElementById("togglePassword");
-    const toggleIcon = togglePasswordButton.querySelector("i");
+    const togglePasswordButtons = document.querySelectorAll(".toggle-password");
 
-    togglePasswordButton.addEventListener("click", function () {
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            toggleIcon.classList.remove("bi-eye-slash");
-            toggleIcon.classList.add("bi-eye");
-        } else {
-            passwordInput.type = "password";
-            toggleIcon.classList.remove("bi-eye");
-            toggleIcon.classList.add("bi-eye-slash");
-        }
+    togglePasswordButtons.forEach((button) => {
+        const passwordInput = document.querySelector(button.getAttribute("data-target"));
+        const toggleIcon = button.querySelector("i");
+
+        button.addEventListener("click", function () {
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                toggleIcon.classList.remove("bi-eye-slash");
+                toggleIcon.classList.add("bi-eye");
+            } else {
+                passwordInput.type = "password";
+                toggleIcon.classList.remove("bi-eye");
+                toggleIcon.classList.add("bi-eye-slash");
+            }
+        });
     });
 });
